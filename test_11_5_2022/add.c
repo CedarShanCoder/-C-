598df3501cf855c9282d73971f886695366a6da4
CedarShanCoder <116875270+CedarShanCoder@users.noreply.github.com>
2022-11-05 11:50:05 +0800

    Add files via upload

---
+++ test_11_5_2022/add.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//全局变量在整个工程中都可以使用
+// static 修饰函数
+//int g_val = 2022;
+//static int g_val = 2022;
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+static int Add(int x, int y)
+{
+	return x + y;
+}
